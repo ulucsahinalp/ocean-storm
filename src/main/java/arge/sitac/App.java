f952e541154cd365aedc743feb56af6effb17796
Uluc Sahinalp <ulucsahinalp@gmail.com>
2014-10-27 16:41:33 +0200

    Initial commit

---
+++ src/main/java/arge/sitac/App.java
@@ -0,0 +1,13 @@
+package arge.sitac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
